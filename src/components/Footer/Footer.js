b155b0c7e4a3cb8d2cbb6dafd04d7a49cc5c89b2
Bruce Van Horn <brucevanhorn@me.com>
2018-06-24 17:22:29 -0500

    Added footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <footer>
+                <p>This is the footer</p>
+            </footer>
+        );
+    }
+}

0e688d5ef3ac8f5f0605dd63fffc599cc3aa29f5
Bruce Van Horn <brucevanhorn@me.com>
2018-06-24 00:31:17 -0500

    Added data structure to app.js which can be passed down into all components.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,45 @@ import SiteCarousel from './components/SiteCarousel';
 import TopNav from './components/TopNav';
 import VehicleBrowser from './components/VehicleBrowser';
 
+const VehicleData = [
+  {
+    model:  "Jumper J-1",
+    modelYear: "2018",
+    colors: ['White Dwarf', 'Red Giant'],
+    carouselImage: '/images/carousel/jumper-01.png',
+    altText: '2018 Jumper J-1',
+    caption: '2018 Jumper J-1',
+    description: '',
+    msrp: 36000
+  },{
+    model:  "Spidi GT",
+    modelYear: "2018",
+    colors: ['White Dwarf', 'Red Giant', 'Blue Nebula', 'Solar Yellow'],
+    carouselImage: '/images/carousel/spidi-01.png',
+    altText: '2018 Spidi - Your Ultimate Getaway Car',
+    caption: '2018 Spidi',
+    description: '',
+    msrp: 42000
+  },{
+    model:  "AirBike",
+    modelYear: "2018",
+    colors: ['White Dwarf', 'Red Giant', 'Black Hole'],
+    carouselImage: '/images/carousel/air-bike-01.png',
+    altText: '2018 AirBike',
+    caption: '2018 AirBike',
+    description: '',
+    msrp: 18500
+  },{
+    model:  "Mercury",
+    modelYear: "2018",
+    colors: ['White Dwarf'],
+    carouselImage: '/images/carousel/mercury-01.png',
+    altText: 'Mercury Spacefaring Commercial Transport',
+    caption: 'Mercurty Spacefaring Commercial Transport',
+    description: '',
+    msrp: 167000
+  }
+]
 class App extends Component {
 
   constructor(props) {

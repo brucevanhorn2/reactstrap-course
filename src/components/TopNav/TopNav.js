018acb494237d2a5dc86bab7c51d377bdf75f9dd
Bruce Van Horn <brucevanhorn@me.com>
2018-06-23 23:00:34 -0500

    created skeleton for top nav component

---
+++ src/components/TopNav/TopNav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class TopNav extends React.Component{
+    constructor(props){
+        super(prop);
+
+        //TODO:  set state
+
+        //TODO:  bind methods
+    }
+
+    render(){
+        return '';
+    }
+}

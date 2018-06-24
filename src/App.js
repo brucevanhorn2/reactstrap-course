e5eee1fcb3a455c18a17012bac72669035bf6c5d
Bruce Van Horn <brucevanhorn@me.com>
2018-06-24 01:08:09 -0500

    Vehicle Browser displays from property data

--- src/App.js
+++ src/App.js
@@ -7,42 +7,46 @@ import VehicleBrowser from './components/VehicleBrowser';
 const VehicleData = [
   {
     model:  "Jumper J-1",
+    tagline: 'Commuting Convenience',
     modelYear: "2018",
     colors: ['White Dwarf', 'Red Giant'],
     carouselImage: '/images/carousel/jumper-01.png',
     altText: '2018 Jumper J-1',
     caption: '2018 Jumper J-1',
-    description: '',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper quam eu velit commodo sagittis. Nam at massa finibus, placerat nulla ac, aliquam mauris. Aenean a libero id odio molestie consectetur id vitae felis. Curabitur vitae lectus consequat, vestibulum orci ut, pulvinar dui. Curabitur cursus cursus tortor gravida tempus. Phasellus eget sagittis lorem. Integer elementum, diam vel sagittis tincidunt, arcu velit sollicitudin mi, eu molestie purus nisl in nulla. Fusce volutpat metus ut metus rhoncus aliquam.',
     detailUrl: '/jumper/',
     msrp: 36000
   },{
     model:  "Spidi GT",
+    tagline: 'Your Ultimate Getaway Car',
     modelYear: "2018",
     colors: ['White Dwarf', 'Red Giant', 'Blue Nebula', 'Solar Yellow'],
     carouselImage: '/images/carousel/spidi-01.png',
     altText: '2018 Spidi - Your Ultimate Getaway Car',
     caption: '2018 Spidi',
-    description: '',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper quam eu velit commodo sagittis. Nam at massa finibus, placerat nulla ac, aliquam mauris. Aenean a libero id odio molestie consectetur id vitae felis. Curabitur vitae lectus consequat, vestibulum orci ut, pulvinar dui. Curabitur cursus cursus tortor gravida tempus. Phasellus eget sagittis lorem. Integer elementum, diam vel sagittis tincidunt, arcu velit sollicitudin mi, eu molestie purus nisl in nulla. Fusce volutpat metus ut metus rhoncus aliquam.',
     detailUrl: '/spidi/',
     msrp: 42000
   },{
     model:  "AirBike",
+    tagline: "New Perspective on Fun",
     modelYear: "2018",
     colors: ['White Dwarf', 'Red Giant', 'Black Hole'],
     carouselImage: '/images/carousel/air-bike-01.png',
     altText: '2018 AirBike',
     caption: '2018 AirBike',
-    description: '',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper quam eu velit commodo sagittis. Nam at massa finibus, placerat nulla ac, aliquam mauris. Aenean a libero id odio molestie consectetur id vitae felis. Curabitur vitae lectus consequat, vestibulum orci ut, pulvinar dui. Curabitur cursus cursus tortor gravida tempus. Phasellus eget sagittis lorem. Integer elementum, diam vel sagittis tincidunt, arcu velit sollicitudin mi, eu molestie purus nisl in nulla. Fusce volutpat metus ut metus rhoncus aliquam.',
     detailUrl: '/airbike/',
     msrp: 18500
   },{
     model:  "Mercury",
+    tagline: "Professional Grade Affordable Fleets.",
     modelYear: "2018",
     colors: ['White Dwarf'],
     carouselImage: '/images/carousel/mercury-01.png',
     altText: 'Mercury Spacefaring Commercial Transport',
     caption: 'Mercurty Spacefaring Commercial Transport',
-    description: '',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper quam eu velit commodo sagittis. Nam at massa finibus, placerat nulla ac, aliquam mauris. Aenean a libero id odio molestie consectetur id vitae felis. Curabitur vitae lectus consequat, vestibulum orci ut, pulvinar dui. Curabitur cursus cursus tortor gravida tempus. Phasellus eget sagittis lorem. Integer elementum, diam vel sagittis tincidunt, arcu velit sollicitudin mi, eu molestie purus nisl in nulla. Fusce volutpat metus ut metus rhoncus aliquam.',
     detailUrl: '/mercury/',
     msrp: 167000
   }
--- src/components/VehicleBrowser/VehicleBrowser.js
+++ src/components/VehicleBrowser/VehicleBrowser.js
@@ -10,21 +10,26 @@ class VehicleBrowser extends React.Component {
     }
 
     render() {
+        const VehicleSelections = this.props.data.map((item) => {
+            return <Col md={ Math.ceil(12 / this.props.data.length) }>
+                <Card className={'VehicleBrowser'}>
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>{item.year} {item.model}</CardTitle>
+                        <CardSubtitle>{item.taglline}</CardSubtitle>
+                        <CardText>{item.description}</CardText>
+                        <Button>Details</Button>
+                        <Button>Build & Price</Button>
+                        <Button>Vehicles Near You</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+        });
         return (
             <div>
                 <Container>
                     <Row>
-                        <Col md="3">
-                            <Card className={'VehicleBrowser'}>
-                                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                                <CardBody>
-                                    <CardTitle>Card title</CardTitle>
-                                    <CardSubtitle>Card subtitle</CardSubtitle>
-                                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                                    <Button>Button</Button>
-                                </CardBody>
-                            </Card>
-                        </Col>
+                        {VehicleSelections}
                     </Row>
                 </Container>
             </div>

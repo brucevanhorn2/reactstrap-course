ed37af7fa9007814bbf5c932fb2ca6985d03e743
Bruce Van Horn <brucevanhorn@me.com>
2018-06-23 22:43:56 -0500

    added skeleton for site carousel component

---
+++ src/components/SiteCarousel/SiteCarosel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SiteCarousel extends React.Component{
+    constructor(props){
+        super(props);
+
+        //TODO:  set the initial state
+
+        //TODO:  bind any methods
+    }
+}

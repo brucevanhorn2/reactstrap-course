b2dfeb66e1c1a97769335fd89e0b02366603b12d
Bruce Van Horn <brucevanhorn@me.com>
2018-06-24 23:40:31 -0500

    Fixed stuff

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,7 +12,7 @@ class Home extends React.Component {
         return (
             <div>
                 <SiteCarousel />
-                <VehicleBrowser />
+                <VehicleBrowser /> 
             </div>);
     }
 }

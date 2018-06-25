1dbb5cd8d2e14e354f544c227486ae21f987cdc9
Bruce Van Horn <brucevanhorn@me.com>
2018-06-25 00:12:07 -0500

    Added color picker

---
+++ src/components/ColorPicker/ColorPicker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ColorPicker extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(<h2>Color Picker</h2>);
+    }
+}
+
+export default ColorPicker;

11dfa32f3f142100a5afcdc2298b8611c8110a3a
Bruce Van Horn <brucevanhorn@me.com>
2018-06-24 23:30:27 -0500

    Enhanced color data

--- src/components/VehicleDetail/VehicleDetail.js
+++ src/components/VehicleDetail/VehicleDetail.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import { Row, Col, Media } from 'reactstrap';
 
-class VehicleDetail extends React.Component{
-    constructor(props){
+class VehicleDetail extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={selectedVehicle: ""}
+        this.state = { selectedVehicle: "" }
     }
 
-    render(){
+    render() {
         const { selectedVehicle } = this.props.match.params;
         const selectedVehicleData = window.data.filter((vehicle) => vehicle.detailKey === selectedVehicle)[0];
 
-    return (<div>
-    <p>This is the vehicle detail screen for {this.state.selectedVehicle}</p>
-        <p>{selectedVehicleData.caption}</p></div>
-    )
+        return (<div>
+            <Row>
+                <Col>
+                    <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=image&w=600&h=400" />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>This is the vehicle detail screen for {this.state.selectedVehicle}</p>
+                    <p>{selectedVehicleData.caption}</p></Col></Row></div>
+        )
     }
 }
 

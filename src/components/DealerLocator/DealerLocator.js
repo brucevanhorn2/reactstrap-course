d5824366d90ebd9551ae9da5c89c0944d8bfc011
Bruce M. Van Horn II <brucevanhorn2@gmail.com>
2018-07-02 13:40:38 -0500

    added form to dealer finder component

--- src/components/DealerLocator/DealerLocator.js
+++ src/components/DealerLocator/DealerLocator.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Row, Col, Media, Form, FormGroup, Label, Input } from 'reactstrap';
 
-class DealerLocator extends React.Component{
-    constructor(props){
+class DealerLocator extends React.Component {
+    constructor(props) {
         super(props);
     }
 
-    render(){
-        return(<h3>Dealer Locator</h3>);
+    render() {
+        return (
+            <div>
+                <h1>Over 500 Authorized Dealers Worldwide</h1>
+                <Row>
+                    <Col sm={12} md={{size: 6, offset: 3}}>
+                        <Form>
+                            <FormGroup>
+                                <Input type="text" name="user_address" placeholder="We're probably nearby.  Type your address." />
+                            </FormGroup>
+                        </Form>
+                    </Col>
+                </Row>
+            </div>
+        );
     }
 }
 

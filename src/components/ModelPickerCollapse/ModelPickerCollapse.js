cc1e2d41a781e8248fafbf70e2912e0c1d8b30be
Bruce Van Horn <brucevanhorn@me.com>
2018-07-12 18:59:00 -0500

    map the vehicles in the selector

--- src/components/ModelPickerCollapse/ModelPickerCollapse.js
+++ src/components/ModelPickerCollapse/ModelPickerCollapse.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './ModelPickerCollapse.css';
-import Holder from 'react-holder';
 import {
     Collapse,
     Media,
@@ -22,30 +21,38 @@ class ModelPickerCollapse extends React.Component {
     }
 
     render() {
-        return (<div className="clickableMedia" onClick={ this.toggle }>
-
-            <Media>
-                <Media left href="#">
-                    <Media className="vehicleImage" src="/images/thumbnails/placeholder96.png" alt="Generic placeholder image" />
-                </Media>
-                <Media body className="vehicleData">
-                    <Media heading className="vehicleTitle">
-                        Media heading
-                    </Media>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                </Media>
-            </Media>
-            <Collapse isOpen={this.state.collapse}>
-                <Card>
-                    <CardBody>
-                        Anim pariatur cliche reprehenderit,
-                         enim eiusmod high life accusamus terry richardson ad squid. Nihil
-                         anim keffiyeh helvetica, craft beer labore wes anderson cred
-                         nesciunt sapiente ea proident.
-            </CardBody>
-                </Card>
-            </Collapse>
-        </div>);
+        return (<div className="clickableMedia" onClick={this.toggle}>
+            {window.data.map(
+                function () {
+                    return (
+                        <div className="vehicleSummary">
+                            <Media>
+                                <Media left href="#">
+                                    <Media className="vehicleImage" src="/images/thumbnails/placeholder96.png" alt="Generic placeholder image" />
+                                </Media>
+                                <Media body className="vehicleData">
+                                    <Media heading className="vehicleTitle">
+                                        Media heading
+                                    </Media>
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                                </Media>
+                            </Media>
+                            <Collapse isOpen={this.state.collapse}>
+                                <Card>
+                                    <CardBody>
+                                        Anim pariatur cliche reprehenderit,
+                                         enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                                         anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                         nesciunt sapiente ea proident.
+                                    </CardBody>
+                                </Card>
+                            </Collapse>
+                        </div>)
+                }
+                , this)}
+            );
+        </div>
+        );
     }
 }
 

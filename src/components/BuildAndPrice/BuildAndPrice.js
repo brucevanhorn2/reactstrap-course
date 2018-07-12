1f990f1a17ab8d949323e0e1b065e57c503dcbd8
Bruce Van Horn <brucevanhorn@me.com>
2018-07-11 23:06:39 -0500

    Added tabs

--- src/components/BuildAndPrice/BuildAndPrice.js
+++ src/components/BuildAndPrice/BuildAndPrice.js
@@ -1,12 +1,97 @@
 import React from 'react';
-
-class BuildAndPrice extends React.Component{
-    constructor(props){
+import classnames from 'classnames';
+import {
+    TabContent,
+    TabPane,
+    Nav,
+    NavItem,
+    NavLink,
+    Card,
+    Button,
+    CardTitle,
+    CardText,
+    Row,
+    Col
+} from 'reactstrap';
+class BuildAndPrice extends React.Component {
+    constructor(props) {
         super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            activeTab: '1'
+        };
     }
+    toggle(tab) {
+        if (tab !== this.state.activeTab) {
+            this.setState({ activeTab: tab });
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h3>Build and Price</h3>
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '1' })}
+                            onClick={() => { this.toggle('1'); }}>
+                            Model
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '2' })}
+                            onClick={() => { this.toggle('2'); }}>
+                            Color
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '3' })}
+                            onClick={() => { this.toggle('3'); }}>
+                            Powerplant
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '4' })}
+                            onClick={() => { this.toggle('4'); }}>
+                            Packages
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent activeTab={this.state.activeTab}>
+                    <TabPane tabId="1">
+                        <Row>
+                            <Col sm="12">
+                                <h4>Tab 1 Contents</h4>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="2">
+                        <Row>
+                            <Col sm="12">
+                                <h4>Tab 2 Contents</h4>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="3">
+                        <Row>
+                            <Col sm="12">
+                                <h4>Tab 3 Contents</h4>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="4">
+                        <Row>
+                            <Col sm="12">
+                                <h4>Tab 4 Contents</h4>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                </TabContent>
 
-    render(){
-        return (<h3>Build and Price</h3>);
+            </div>);
     }
 }
 

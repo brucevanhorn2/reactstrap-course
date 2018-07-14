73c6ca678533bb44938686922f506361148d8ffc
Bruce Van Horn <brucevanhorn@me.com>
2018-07-14 14:10:19 -0500

    Added table code so the thumbnails would line up instead of stacking.

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -12,16 +12,25 @@ class ColorPicker extends React.Component {
         if (selectedVehicleData) {
             return (
                 <div className="colorBox">
+                <table>
+                    <tbody>
+                    <tr>
                     {selectedVehicleData.colors.map(
                         function (color, i) {
-                            return <img key={"colorpicker-" + color[1]}
+                            return <td>
+                            <img key={"colorpicker-" + color[1]}
                                 className="colorChip"
                                 alt={color[0]}
                                 onClick={this.props.selectColor}
                                 data-color={i}
-                                src={color[1]} />
+                                src={color[1]} /><br/>
+                                <span>{color[0]}</span>
+                            </td>
                         }, this
                     )}
+                    </tr>
+                    </tbody>
+                    </table>
                 </div>
             );
         } else {

7dad12209808949dce6e5456d9db188c1377677d
Bruce Van Horn <brucevanhorn2@gmail.com>
2018-07-16 16:20:14 -0500

    stub for form component

---
+++ src/components/TestFlightForm/TestFlightForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TestFlightForm.css';
+
+class TestFlightForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(<h4>Form goes here</h4>);
+    }
+}
+
+export default TestFlightForm;

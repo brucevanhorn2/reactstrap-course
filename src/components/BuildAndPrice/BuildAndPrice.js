7d115fa790e9f662c7ec96a4a762d87816beeb62
Bruce Van Horn <brucevanhorn2@gmail.com>
2018-07-16 13:15:22 -0500

    added image carousel - it works but doesn't show all the thumbnails yet.

--- src/components/BuildAndPrice/BuildAndPrice.js
+++ src/components/BuildAndPrice/BuildAndPrice.js
@@ -4,6 +4,7 @@ import './BuildAndPrice.css';
 import ColorPicker from '../ColorPicker/ColorPicker';
 import ModelPicker from '../ModelPicker/ModelPicker';
 import EnginePicker from '../EngineSelector/EngineSelector';
+import BuildAndPriceImageRotator from '../BuildAndPriceImageRotator/BuildAndPriceImageRotator';
 import {
     TabContent,
     TabPane,
@@ -31,7 +32,7 @@ class BuildAndPrice extends React.Component {
         this.toggleModal = this.toggleModal.bind(this);
         this.state = {
             activeTab: '1',  //currently displayed tab (note it starts at 1 not 0)
-            selectedVehicle: "", //holds the key to the selected vehicle
+            selectedVehicle: "jumper", //holds the key to the selected vehicle
             selectedColor: 0, //holds the selected color
             selectedEngine: 0,  //holds the array index of the selected engine option
             modal: false, //controls the appearance of the modal
@@ -56,10 +57,9 @@ class BuildAndPrice extends React.Component {
 
     selectEngine(eventData){
         const selectedEngine = eventData.target.getAttribute('data-engine');
-        console.log("the selected engine is " + selectedEngine);
         this.setState({
             selectedEngine: Number(selectedEngine), modal: true
-        })
+        });
     }
 
     toggleModal(){
@@ -73,68 +73,71 @@ class BuildAndPrice extends React.Component {
     }
     render() {
         return (
-            <div className="tabPanel">
-                <h3>Build and Price</h3>
-                <Progress color="primary" value="50" />
-                <Nav tabs>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: this.state.activeTab === '1' })}
-                            onClick={() => { this.toggle('1'); }}>
-                            Model
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: this.state.activeTab === '2' })}
-                            onClick={() => { this.toggle('2'); }}>
-                            Color
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: this.state.activeTab === '3' })}
-                            onClick={() => { this.toggle('3'); }}>
-                            Powerplant
-                        </NavLink>
-                    </NavItem>
-                    
-                </Nav>
-                <TabContent activeTab={this.state.activeTab}>
-                    <TabPane tabId="1">
-                        <Row>
-                            <Col sm="12">
-                                <ModelPicker selectVehicle={this.selectVehicle} />
-                            </Col>
-                        </Row>
-                    </TabPane>
-                    <TabPane tabId="2">
-                        <Row>
-                            <Col sm="12">
-                            <ColorPicker
-                                selectColor = {this.selectColor}
-                                selectedVehicle = {this.state.selectedVehicle} />
-                            </Col>
-                        </Row>
-                    </TabPane>
-                    <TabPane tabId="3">
-                        <Row>
-                            <Col sm="12">
-                                <EnginePicker onEngineSelect = {this.selectEngine} selectedVehicle = {this.state.selectedVehicle} />
-                            </Col>
-                        </Row>
-                    </TabPane>
-                    
-                </TabContent>
-                <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}>Check Out Your New Ride!</ModalHeader>
-                    <ModalBody>
-                        Data go here
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="primary" onClick = {this.toggleModal}>Done!</Button>
-                    </ModalFooter>
-                </Modal>
+            <div>
+                <BuildAndPriceImageRotator selectedVehicle={this.state.selectedVehicle} />
+                <div className="tabPanel">
+                    <h3>Build and Price</h3>
+                    <Progress color="primary" value="50" />
+                    <Nav tabs>
+                        <NavItem>
+                            <NavLink
+                                className={classnames({ active: this.state.activeTab === '1' })}
+                                onClick={() => { this.toggle('1'); }}>
+                                Model
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink
+                                className={classnames({ active: this.state.activeTab === '2' })}
+                                onClick={() => { this.toggle('2'); }}>
+                                Color
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink
+                                className={classnames({ active: this.state.activeTab === '3' })}
+                                onClick={() => { this.toggle('3'); }}>
+                                Powerplant
+                            </NavLink>
+                        </NavItem>
+                        
+                    </Nav>
+                    <TabContent activeTab={this.state.activeTab}>
+                        <TabPane tabId="1">
+                            <Row>
+                                <Col sm="12">
+                                    <ModelPicker selectVehicle={this.selectVehicle} />
+                                </Col>
+                            </Row>
+                        </TabPane>
+                        <TabPane tabId="2">
+                            <Row>
+                                <Col sm="12">
+                                <ColorPicker
+                                    selectColor = {this.selectColor}
+                                    selectedVehicle = {this.state.selectedVehicle} />
+                                </Col>
+                            </Row>
+                        </TabPane>
+                        <TabPane tabId="3">
+                            <Row>
+                                <Col sm="12">
+                                    <EnginePicker onEngineSelect = {this.selectEngine} selectedVehicle = {this.state.selectedVehicle} />
+                                </Col>
+                            </Row>
+                        </TabPane>
+                        
+                    </TabContent>
+                    <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}>Check Out Your New Ride!</ModalHeader>
+                        <ModalBody>
+                            Data go here
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="primary" onClick = {this.toggleModal}>Done!</Button>
+                        </ModalFooter>
+                    </Modal>
+                </div>
             </div>);
     }
 }
--- src/components/BuildAndPriceImageRotator/BuildAndPriceImageRotator.js
+++ src/components/BuildAndPriceImageRotator/BuildAndPriceImageRotator.js
@@ -1,13 +1,87 @@
 import React from 'react';
-import './BuildAndPriceImageRotator.css'
+import './BuildAndPriceImageRotator.css';
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption
+  } from 'reactstrap';
+
+
+
 class BuildAndPriceImageRotator extends React.Component {
     constructor(props){
         super(props);
+        this.state = { activeIndex: 0 };
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.goToIndex = this.goToIndex.bind(this);
+        this.onExiting = this.onExiting.bind(this);
+        this.onExited = this.onExited.bind(this);
+
+        this.selectedVehicleData = window.data.filter((vehicle) => vehicle.detailKey === this.props.selectedVehicle)[0];
+        this.items = this.selectedVehicleData.colors.map(function(colorData, i){
+            return {
+                src: colorData[1],
+                altText: colorData[0],
+                caption: colorData[0]
+            }
+        });
     }
 
+    onExiting() {
+        this.animating = true;
+      }
+    
+      onExited() {
+        this.animating = false;
+      }
+    
+      next() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === this.items.length - 1 ? 0 : this.state.activeIndex + 1;
+        this.setState({ activeIndex: nextIndex });
+      }
+    
+      previous() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === 0 ? this.items.length - 1 : this.state.activeIndex - 1;
+        this.setState({ activeIndex: nextIndex });
+      }
+    
+      goToIndex(newIndex) {
+        if (this.animating) return;
+        this.setState({ activeIndex: newIndex });
+      }
+
     render(){
-        <h4>Image will be here</h4>
-    }
+        const { activeIndex } = this.state;
+        
+        const slides = this.items.map((item) => {
+            return (
+                <CarouselItem
+                  onExiting={this.onExiting}
+                  onExited={this.onExited}
+                  key={item.src}>
+                <img className={"buildAndPriceImage"} src={item.src} alt={item.altText} />
+                    <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+                </CarouselItem>
+            );
+        });
+
+        return (
+            <Carousel
+                activeIndex={activeIndex}
+                next={this.next}
+                previous={this.previous}>
+                <CarouselIndicators items={this.items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+                {slides}
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+            </Carousel>
+        );
+  }
 }
 
 export default BuildAndPriceImageRotator;

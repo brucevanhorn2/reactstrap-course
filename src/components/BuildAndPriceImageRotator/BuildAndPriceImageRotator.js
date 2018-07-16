00e89679596cbcc6a7a22fd6442eb1c968de3cb2
Bruce Van Horn <brucevanhorn2@gmail.com>
2018-07-16 10:48:50 -0500

    added image rotator component for build and price

---
+++ src/components/BuildAndPriceImageRotator/BuildAndPriceImageRotator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BuildAndPriceImageRotator.css'
+class BuildAndPriceImageRotator extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        <h4>Image will be here</h4>
+    }
+}
+
+export default BuildAndPriceImageRotator;

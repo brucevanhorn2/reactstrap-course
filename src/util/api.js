0c8f5b1bc0b67ed26e853893ad49a0c433d84222
Bruce Van Horn <brucevanhorn@me.com>
2018-07-17 17:58:34 -0500

    added verbs code (just get and post)

--- src/util/api.js
+++ src/util/api.js
@@ -1,3 +1,40 @@
-//need to add my api calls here in normal functions
-//store the data in local storage
-//check to see if its there.  if it is, use it.  if it isn't , query it and store.
+export default {
+    doGet: (endPoint, callback) => {
+        const headers = {'Cache-Control': 'no-cache'};
+        const urlConfig = {
+            method: "GET",
+            mode: 'cors',
+            headers: headers
+        };
+        fetch(endPoint, urlConfig).then(
+            response => {
+                return response.json();
+            }).catch(error => {
+                console.log("Error: " + error);
+                if(typeof (callback) === "function") {
+                    callback(error);
+                }
+             }).then(data => {
+            if (data) {
+                callback(data);
+            }
+        });
+    },
+    doPost: (endPoint, formData, callback) => {
+        const headers = {'Cache-Control': 'no-cache'};
+        const urlConfig = {
+            method: "POST",
+            mode: 'cors',
+            headers: headers,
+            body: formData
+        };
+        fetch(endPoint, urlConfig).then(
+            response => {
+                return response.json();
+            }).catch(error => {
+                callback(error);
+            }).then(data => {
+                callback(data);
+            });
+    }
+}

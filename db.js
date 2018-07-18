dddf9192848a07bc504974ad247424c8d172a8e4
Bruce Van Horn <brucevanhorn2@gmail.com>
2018-07-18 14:40:38 -0500

    Converted db.json into plain js and added it to the generate script

---
+++ db.js
@@ -0,0 +1,169 @@
+//this is a js version of the db.json file
+//VS Code will show errors in it, but that's OK - we just use it to
+//copy / paste the vehicles data.
+{
+    vehicles: [{
+        model: "Jumper J-1",
+        tagline: "Commuting Convenience",
+        modelYear: "2018",
+        thumbnail: "/images/thumbnails/jumper/jumper-red-thumbnails00.png",
+        colors: [
+            ["White Dwarf", "/images/thumbnails/jumper/jumper-white-thumbnails10.png", "white"],
+            ["Red Giant", "/images/thumbnails/jumper/jumper-red-thumbnails10.png", "red"]
+        ],
+        carouselImage: "/images/carousel/jumper-01.png",
+        altText: "2018 Jumper J-1",
+        caption: "2018 Jumper J-1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper quam eu velit commodo sagittis. Nam at massa finibus, placerat nulla ac, aliquam mauris. Aenean a libero id odio molestie consectetur id vitae felis. Curabitur vitae lectus consequat, vestibulum orci ut, pulvinar dui. Curabitur cursus cursus tortor gravida tempus. Phasellus eget sagittis lorem. Integer elementum, diam vel sagittis tincidunt, arcu velit sollicitudin mi, eu molestie purus nisl in nulla. Fusce volutpat metus ut metus rhoncus aliquam.",
+        detailKey: "jumper",
+        msrp: 36000,
+        options: {
+            engines: [{
+                name: "Alpha Centauri Mark II",
+                image: "/images/engines/engine1.png",
+                cost: 0,
+                nmpg: 75,
+                thrust: "245 kN",
+                topspeed: 20
+            }, {
+                name: "Singularity Engine Mark V",
+                image: "/images/engines/engine2.png",
+                cost: 2250,
+                nmpg: 55,
+                thrust: "545 kN",
+                topspeed: 60
+            }]
+        }
+    }, {
+        model: "Spidi GT",
+        tagline: "Your Ultimate Getaway Car",
+        modelYear: "2018",
+        thumbnail: "/images/thumbnails/spidi/spidi-blue-thumbnails00.png",
+        colors: [
+            ["White Dwarf", "/images/thumbnails/spidi/spidi-white-thumbnails10.png", "white"],
+            ["Black Hole", "/images/thumbnails/spidi/spidi-black-thumbnails10.png", "black"],
+            ["Red Giant", "/images/thumbnails/spidi/spidi-red-thumbnails10.png", "red"],
+            ["Main Sequence Blue", "/images/thumbnails/spidi/spidi-blue-thumbnails10.png", "blue"],
+            ["Solar Yellow", "/images/thumbnails/spidi/spidi-yellow-thumbnails10.png", "yellow"]
+        ],
+        carouselImage: "/images/carousel/spidi-01.png",
+        altText: "2018 Spidi - Your Ultimate Getaway Car",
+        caption: "2018 Spidi",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper quam eu velit commodo sagittis. Nam at massa finibus, placerat nulla ac, aliquam mauris. Aenean a libero id odio molestie consectetur id vitae felis. Curabitur vitae lectus consequat, vestibulum orci ut, pulvinar dui. Curabitur cursus cursus tortor gravida tempus. Phasellus eget sagittis lorem. Integer elementum, diam vel sagittis tincidunt, arcu velit sollicitudin mi, eu molestie purus nisl in nulla. Fusce volutpat metus ut metus rhoncus aliquam.",
+        detailKey: "spidi",
+        msrp: 42000,
+        options: {
+            engines: [{
+                name: "Boson Accelerator Mark III",
+                image: "/images/engine3.png",
+                cost: 0,
+                nmpg: 75,
+                thrust: "245 kN",
+                topspeed: 20
+            }, {
+                name: "Singularity Engine Mark V",
+                image: "/images/engines/engine2.png",
+                cost: 2250,
+                nmpg: 55,
+                thrust: "545 kN",
+                topspeed: 60
+            }]
+        }
+    }, {
+        model: "AirBike",
+        tagline: "New Perspective on Fun",
+        modelYear: "2018",
+        thumbnail: "/images/thumbnails/air-bike/air-bike-purple-thumbnails00.png",
+        colors: [
+            ["Nebula Purple", "/images/thumbnails/air-bike/air-bike-purple-thumbnails10.png", "purple"],
+            ["Main Sequence Blue", "/images/thumbnails/air-bike/air-bike-blue-thumbnails10.png", "blue"],
+            ["Black Hole", "/images/thumbnails/air-bike/air-bike-black-thumbnails10.png", "black"],
+            ["Solar Yellow", "/images/thumbnails/air-bike/air-bike-yellow-thumbnails10.png", "yellow"]
+        ],
+        carouselImage: "/images/carousel/air-bike-01.png",
+        altText: "2018 AirBike",
+        caption: "2018 AirBike",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper quam eu velit commodo sagittis. Nam at massa finibus, placerat nulla ac, aliquam mauris. Aenean a libero id odio molestie consectetur id vitae felis. Curabitur vitae lectus consequat, vestibulum orci ut, pulvinar dui. Curabitur cursus cursus tortor gravida tempus. Phasellus eget sagittis lorem. Integer elementum, diam vel sagittis tincidunt, arcu velit sollicitudin mi, eu molestie purus nisl in nulla. Fusce volutpat metus ut metus rhoncus aliquam.",
+        detailKey: "air-bike",
+        msrp: 18500,
+        options: {
+            engines: [{
+                name: "Alpha Centauri Mark II",
+                image: "/images/engines/engine1.png",
+                cost: 0,
+                nmpg: 75,
+                thrust: "245 kN",
+                topspeed: 20
+            }, {
+                name: "Singularity Engine Mark V",
+                image: "/images/engines/engine2.png",
+                cost: 2250,
+                nmpg: 55,
+                thrust: "545 kN",
+                topspeed: 60
+            }]
+        }
+    }, {
+        model: "Mercury",
+        tagline: "Professional Grade Affordable Fleets.",
+        modelYear: "2018",
+        thumbnail: "/images/thumbnails/mercury/mercury-white-thumbnails00.png",
+        colors: [
+            ["White Dwarf", "/images/thumbnails/mercury-white.png", "white"]
+        ],
+        carouselImage: "/images/carousel/mercury-01.png",
+        altText: "Mercury Spacefaring Commercial Transport",
+        caption: "Mercury Spacefaring Commercial Transport",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper quam eu velit commodo sagittis. Nam at massa finibus, placerat nulla ac, aliquam mauris. Aenean a libero id odio molestie consectetur id vitae felis. Curabitur vitae lectus consequat, vestibulum orci ut, pulvinar dui. Curabitur cursus cursus tortor gravida tempus. Phasellus eget sagittis lorem. Integer elementum, diam vel sagittis tincidunt, arcu velit sollicitudin mi, eu molestie purus nisl in nulla. Fusce volutpat metus ut metus rhoncus aliquam.",
+        detailKey: "mercury",
+        msrp: 167000,
+        options: {
+            engines: [{
+                name: "Mercury Mark VI",
+                image: "/images/engines/engine4.png",
+                cost: 0,
+                nmpg: 75,
+                thrust: "245 kN",
+                topspeed: 20
+            }]
+        }
+    }],
+    dealerships: [{
+        dealershipName: "Slick Willy's Flying Cars",
+        address: "555 Nearby St.  Ada, Oklahoma 74820",
+        phone: "1-800-fly-cars"
+    }, {
+        dealershipName: "Dewey, Cheatham, &amp; Howe Auto Sales",
+        address: "123 Test St. Dallas Texas, 75001",
+        phone: "1-800-pay-here"
+    }, {
+        dealershipName: "Ima Crooke Flying Car Sales",
+        address: "234 Booyah Ln. San Mateo California, 75001",
+        phone: "1-888-fak-enum"
+    }],
+    mailingList: [{
+        customerName: "Bruce Van Horn",
+        email: "actual@contentfour.com",
+        budget: "500000",
+        phone: "9728036394",
+        id: 1
+    }, {
+        customerName: "Karina Van Horn",
+        email: "test@test.com",
+        budget: "50001",
+        phone: "666-2345",
+        id: 2
+    }, {
+        customerName: "test",
+        email: "test@test.com",
+        budget: "50000",
+        phone: "5552345",
+        id: 3
+    }, {
+        customerName: "test",
+        email: "test@test.com",
+        budget: "500000",
+        phone: "test",
+        id: 4
+    }]
+}
--- generate.js
+++ generate.js
@@ -3,7 +3,132 @@ module.exports = function(){
     var _ = require("lodash");
     return {
         mailingList: [],
-        vehicles: [],
+        vehicles: [{
+            model: "Jumper J-1",
+            tagline: "Commuting Convenience",
+            modelYear: "2018",
+            thumbnail: "/images/thumbnails/jumper/jumper-red-thumbnails00.png",
+            colors: [
+                ["White Dwarf", "/images/thumbnails/jumper/jumper-white-thumbnails10.png", "white"],
+                ["Red Giant", "/images/thumbnails/jumper/jumper-red-thumbnails10.png", "red"]
+            ],
+            carouselImage: "/images/carousel/jumper-01.png",
+            altText: "2018 Jumper J-1",
+            caption: "2018 Jumper J-1",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper quam eu velit commodo sagittis. Nam at massa finibus, placerat nulla ac, aliquam mauris. Aenean a libero id odio molestie consectetur id vitae felis. Curabitur vitae lectus consequat, vestibulum orci ut, pulvinar dui. Curabitur cursus cursus tortor gravida tempus. Phasellus eget sagittis lorem. Integer elementum, diam vel sagittis tincidunt, arcu velit sollicitudin mi, eu molestie purus nisl in nulla. Fusce volutpat metus ut metus rhoncus aliquam.",
+            detailKey: "jumper",
+            msrp: 36000,
+            options: {
+                engines: [{
+                    name: "Alpha Centauri Mark II",
+                    image: "/images/engines/engine1.png",
+                    cost: 0,
+                    nmpg: 75,
+                    thrust: "245 kN",
+                    topspeed: 20
+                }, {
+                    name: "Singularity Engine Mark V",
+                    image: "/images/engines/engine2.png",
+                    cost: 2250,
+                    nmpg: 55,
+                    thrust: "545 kN",
+                    topspeed: 60
+                }]
+            }
+        }, {
+            model: "Spidi GT",
+            tagline: "Your Ultimate Getaway Car",
+            modelYear: "2018",
+            thumbnail: "/images/thumbnails/spidi/spidi-blue-thumbnails00.png",
+            colors: [
+                ["White Dwarf", "/images/thumbnails/spidi/spidi-white-thumbnails10.png", "white"],
+                ["Black Hole", "/images/thumbnails/spidi/spidi-black-thumbnails10.png", "black"],
+                ["Red Giant", "/images/thumbnails/spidi/spidi-red-thumbnails10.png", "red"],
+                ["Main Sequence Blue", "/images/thumbnails/spidi/spidi-blue-thumbnails10.png", "blue"],
+                ["Solar Yellow", "/images/thumbnails/spidi/spidi-yellow-thumbnails10.png", "yellow"]
+            ],
+            carouselImage: "/images/carousel/spidi-01.png",
+            altText: "2018 Spidi - Your Ultimate Getaway Car",
+            caption: "2018 Spidi",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper quam eu velit commodo sagittis. Nam at massa finibus, placerat nulla ac, aliquam mauris. Aenean a libero id odio molestie consectetur id vitae felis. Curabitur vitae lectus consequat, vestibulum orci ut, pulvinar dui. Curabitur cursus cursus tortor gravida tempus. Phasellus eget sagittis lorem. Integer elementum, diam vel sagittis tincidunt, arcu velit sollicitudin mi, eu molestie purus nisl in nulla. Fusce volutpat metus ut metus rhoncus aliquam.",
+            detailKey: "spidi",
+            msrp: 42000,
+            options: {
+                engines: [{
+                    name: "Boson Accelerator Mark III",
+                    image: "/images/engine3.png",
+                    cost: 0,
+                    nmpg: 75,
+                    thrust: "245 kN",
+                    topspeed: 20
+                }, {
+                    name: "Singularity Engine Mark V",
+                    image: "/images/engines/engine2.png",
+                    cost: 2250,
+                    nmpg: 55,
+                    thrust: "545 kN",
+                    topspeed: 60
+                }]
+            }
+        }, {
+            model: "AirBike",
+            tagline: "New Perspective on Fun",
+            modelYear: "2018",
+            thumbnail: "/images/thumbnails/air-bike/air-bike-purple-thumbnails00.png",
+            colors: [
+                ["Nebula Purple", "/images/thumbnails/air-bike/air-bike-purple-thumbnails10.png", "purple"],
+                ["Main Sequence Blue", "/images/thumbnails/air-bike/air-bike-blue-thumbnails10.png", "blue"],
+                ["Black Hole", "/images/thumbnails/air-bike/air-bike-black-thumbnails10.png", "black"],
+                ["Solar Yellow", "/images/thumbnails/air-bike/air-bike-yellow-thumbnails10.png", "yellow"]
+            ],
+            carouselImage: "/images/carousel/air-bike-01.png",
+            altText: "2018 AirBike",
+            caption: "2018 AirBike",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper quam eu velit commodo sagittis. Nam at massa finibus, placerat nulla ac, aliquam mauris. Aenean a libero id odio molestie consectetur id vitae felis. Curabitur vitae lectus consequat, vestibulum orci ut, pulvinar dui. Curabitur cursus cursus tortor gravida tempus. Phasellus eget sagittis lorem. Integer elementum, diam vel sagittis tincidunt, arcu velit sollicitudin mi, eu molestie purus nisl in nulla. Fusce volutpat metus ut metus rhoncus aliquam.",
+            detailKey: "air-bike",
+            msrp: 18500,
+            options: {
+                engines: [{
+                    name: "Alpha Centauri Mark II",
+                    image: "/images/engines/engine1.png",
+                    cost: 0,
+                    nmpg: 75,
+                    thrust: "245 kN",
+                    topspeed: 20
+                }, {
+                    name: "Singularity Engine Mark V",
+                    image: "/images/engines/engine2.png",
+                    cost: 2250,
+                    nmpg: 55,
+                    thrust: "545 kN",
+                    topspeed: 60
+                }]
+            }
+        }, {
+            model: "Mercury",
+            tagline: "Professional Grade Affordable Fleets.",
+            modelYear: "2018",
+            thumbnail: "/images/thumbnails/mercury/mercury-white-thumbnails00.png",
+            colors: [
+                ["White Dwarf", "/images/thumbnails/mercury-white.png", "white"]
+            ],
+            carouselImage: "/images/carousel/mercury-01.png",
+            altText: "Mercury Spacefaring Commercial Transport",
+            caption: "Mercury Spacefaring Commercial Transport",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ullamcorper quam eu velit commodo sagittis. Nam at massa finibus, placerat nulla ac, aliquam mauris. Aenean a libero id odio molestie consectetur id vitae felis. Curabitur vitae lectus consequat, vestibulum orci ut, pulvinar dui. Curabitur cursus cursus tortor gravida tempus. Phasellus eget sagittis lorem. Integer elementum, diam vel sagittis tincidunt, arcu velit sollicitudin mi, eu molestie purus nisl in nulla. Fusce volutpat metus ut metus rhoncus aliquam.",
+            detailKey: "mercury",
+            msrp: 167000,
+            options: {
+                engines: [{
+                    name: "Mercury Mark VI",
+                    image: "/images/engines/engine4.png",
+                    cost: 0,
+                    nmpg: 75,
+                    thrust: "245 kN",
+                    topspeed: 20
+                }]
+            }
+        }],
         dealerships: _.times(100, function(n){
             return{
                 id: n,
